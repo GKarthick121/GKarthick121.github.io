f5eb11dce0907ac0e52e0f6047cb9155b41f2f29
GKarthick121 <58224677+GKarthick121@users.noreply.github.com>
2019-12-26 19:21:24 +0000

    Update route-animations.ts

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
-// import the required animation functions from the angular animations module
-import { trigger, state, animate, transition, style } from '@angular/animations';
+// import the required animation functions
+import { trigger, state, animate, transition, style,query } from '@angular/animations';
 
 export const slideInOutAnimation =
+    // trigger name for attaching this animation
     trigger('slideInOutAnimation', [
+      transition('* <=> *', [
+        query(':enter, :leave', [
         state('*', style({
             position: 'fixed',
             top: 0,
             left: 0,
             right: 0,
             bottom: 0,
-        })),
-        transition(':enter', [ style({ right: '-400%', }),
-       
-        animate('.5s ease-in-out', style({right: 0, }))
-         ]),     
-                
+        })), ]),
+        transition(':enter', [
 
-        transition(':leave', [animate('.5s ease-in-out', style({right: '-400%', }))]
-        
+            style({
+                 right: '-400%',
+            }),
+
+            // animation and styles at end of transition
+            animate('1s ease-in-out', style({
+           right: 0,
+            }))
+        ]),
+
+        // route 'leave' transition
+        transition(':leave', [
+            animate('1s ease-in-out', style({
+                right: '-400%',
+            }))
+        ])
+      ]),
     ]);
-
